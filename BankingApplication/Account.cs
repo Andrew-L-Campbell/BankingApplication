629cf89351ef1b8505792360f373d74f72d5857d
Andrew-L-Campbell <83891416+Andrew-L-Campbell@users.noreply.github.com>
2023-02-07 13:11:39 -0700

    Banking application with validation

---
+++ BankingApplication/Account.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApplication
+{
+    enum AccountState
+    {
+        /// <summary>
+        /// This enum contains all the states of the account
+        /// </summary>
+        New,
+        Active,
+        UnderAudit,
+        Frozen,
+        Closed
+    }
+    internal abstract class Account : IAccount
+    ///<summary>
+    /// This class models a bank account. It contains key information
+    /// about a account and methods to manage that data/// </summary>
+    {
+        public string name;//variables
+        public string address;
+        public string accountNumber;
+        public decimal balance;
+        const int InitialBalance = 100;
+        public AccountState state;//enum variable
+        public static int incAccNum = 1000;
+
+        public Account()
+        /// <summary>
+        /// Purpose: Constructor to initialize account with set state and balance
+        /// </summary>
+        /// <param state="AccountState.New">setting to New enum</param>
+        /// <param balance="InitialBalance">starting out all account holders with 100$</param>
+
+        {
+            state = AccountState.New;
+            balance = InitialBalance;
+
+        }
+        public override string ToString()
+        /// <summary>
+        /// Purpose: To override the printing of the account to a customized one
+        /// </summary>
+        /// <returns>A structured output string</returns>
+        {
+            return
+                $"Account Holder's Name: {name}\n" +
+                $"Address: {address}\n" +
+                $"Account Number: {accountNumber}\n" +
+                $"Current Balance: {balance}\n" +
+                $"State: {state}";
+        }
+
+        public bool SetName(string inName)
+        /// <summary>
+        /// Purpose: To set the account holders name
+        /// </summary>
+        /// <param name="inName">any valid string</param>
+        /// <returns>boolean</returns>
+        {
+            name = inName;
+            if (name == null || name == "")
+            {
+                return false;
+            }
+            else { return true; }
+        }
+
+        public string GetName()
+        {
+            /// <summary>
+            /// Purpose: To return the account holder name
+            /// </summary>
+            /// <returns>name</returns>
+            return name;
+        }
+
+        public bool SetAddress(string inAddress)
+        /// <summary>
+        /// Purpose: To set account holders address
+        /// </summary>
+        /// <param address="inAddress">any valid string value</param>
+        /// <returns>boolean</returns>
+        {
+            address = inAddress;
+            if (address == null || address == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string GetAddress()
+        /// <summary>
+        /// Purpose: To address of account holder
+        /// </summary>
+        /// <returns>address</returns>
+        {
+            return address;
+        }
+
+        public bool PayInFunds(decimal amount)
+        /// <summary>
+        /// Purpose: To deposit funds into account balance
+        /// </summary>
+        /// <param amount>any valid decimal value thats not negative</param>
+
+        /// <returns>balance + amount</returns>
+        {
+            if (amount < 0)
+            {
+                return false;
+            }
+            balance = amount + balance;
+            return true;
+        }
+
+        public bool WithdrawFunds(decimal amount)
+        /// <summary>
+        /// Purpose: To withdraw money from account balance
+        /// </summary>
+        /// <param amount>any valid decimal value thats not more than whats in the account</param>
+        /// <returns>balance - amount</returns>
+        {
+            if (balance - amount < 0)
+            {
+                return false;
+            }
+            balance = balance - amount;
+            return true;
+        }
+
+        public bool SetBalance(decimal inBalance)
+        /// <summary>
+        /// Purpose: To set the account balance
+        /// </summary>
+        /// <param inBalance="balance">any valid double value >= 0</param>
+        /// <returns>boolean</returns>
+        {
+            if (inBalance < 0) { return false; }
+            balance = inBalance;
+            return true;
+        }
+
+        public decimal GetBalance()
+        /// <summary>
+        /// Purpose: To return the account balance
+        /// </summary>
+        /// <returns>balance</returns>
+        {
+            return balance;
+        }
+
+        public void SetState(AccountState state)
+        /// <summary>
+        /// Purpose: To return the accounts state
+        /// </summary>
+        /// <param AccountState="state">any enum declared in AccountState</param>
+        {
+            this.state = state;
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////
+    internal class CheckingAccount : Account
+    ///<summary>
+    /// This class models checking account whose parent is account/// </summary>
+    {
+        public decimal _accountServiceFee;
+        public CheckingAccount()
+        {
+        }
+        public void AccountNumber()
+        /// <summary>
+        /// Purpose: set the account number depending on this account type
+        {
+
+            incAccNum = incAccNum + 1;
+            accountNumber = incAccNum.ToString()+ "C";
+        }
+        public decimal accountServiceFee
+        /// <summary>
+        /// Purpose: get and set the account service fee depending on this account type
+        /// Returns: the service fee amount.
+        {
+            get {
+                return _accountServiceFee;
+            }
+            set {
+                _accountServiceFee = value;
+            }
+        }
+        public bool CheckSetBalance(decimal inBalance)
+        /// <summary>
+        /// Purpose: checks the initial balance to make sure there is enout put in
+        /// parameters: initial deposit amount
+        /// Returns: true or false
+
+        {
+            if (inBalance < 10) {
+                Console.WriteLine("Starting Balance must be at least $500, please try again.");
+                return false; }
+            balance = inBalance;
+            return true;
+        }
+    }
+    internal class SavingAccount : Account
+    ///<summary>
+    /// This class models a savings account whose parent is the account class/// </summary>
+    {
+        public decimal _accountServiceFee;
+   
+        public void AccountNumber()
+        /// <summary>
+        /// Purpose: set the account number depending on this account type
+        {
+            incAccNum = incAccNum + 1;
+            accountNumber = incAccNum.ToString() + "S";
+        }
+        public decimal accountServiceFee
+        /// <summary>
+        /// Purpose: get and set the account service fee depending on this account type
+        /// Returns: the service fee amount.
+        {
+            get
+            {
+                return _accountServiceFee;
+            }
+            set
+            {
+                _accountServiceFee = value;
+            }
+        }
+        public bool SaveSetBalance(decimal inBalance)
+        /// <summary>
+        /// Purpose: checks the initial balance to make sure there is enout put in
+        /// parameters: initial deposit amount
+        /// Returns: true or false
+
+        {
+            if (inBalance < 100) {
+                Console.WriteLine("Starting Balance must be at least $500, please try again.");
+                return false; }
+            balance = inBalance;
+            return true;
+        }
+    }
+    internal class CDAccount : Account
+    ///<summary>
+    /// This class models a CD account whose parent is the account class/// </summary>
+    {
+        public decimal _accountServiceFee;
+
+        public void AccountNumber()
+        /// <summary>
+        /// Purpose: set the account number depending on this account type
+        {
+            incAccNum = incAccNum + 1;
+            accountNumber = incAccNum.ToString() + "D";
+        }
+        public decimal accountServiceFee
+        /// <summary>
+        /// Purpose: get and set the account service fee depending on this account type
+        /// Returns: the service fee amount.
+        {
+            get
+            {
+                return _accountServiceFee;
+            }
+            set
+            {
+                _accountServiceFee = value;
+            }
+        }
+        public bool CDSetBalance(decimal inBalance)
+        /// <summary>
+        /// Purpose: checks the initial balance to make sure there is enout put in
+        /// parameters: initial deposit amount
+        /// Returns: true or false
+        {
+            if (inBalance < 500) {
+                Console.WriteLine("Starting Balance must be at least $500, please try again.\n");
+                return false; }
+            balance = inBalance;
+            return true;
+        }
+    }
+    internal class AccountManager
+    ///<summary>
+    /// This class models a account manager. Its used to store the accounts/// </summary>
+    {
+        public static int accountCount = 0;
+        Account[] accounts;
+
+        public AccountManager()
+        {
+
+        }
+        public void SetArrayLength(int size) {
+            /// <summary>
+            /// Purpose: helper method to set array size
+            /// parameters: size its to be
+            accounts = new Account[size];
+        }
+        public bool StoreAccount(Account inAccount) {
+            /// <summary>
+            /// Purpose: stores the account in a array within accountmanager
+            /// parameters: a created account
+            /// Returns: true or false
+            if (inAccount == null) {
+                Console.WriteLine("Object is null, please fix and try again\n");
+                return false;
+            }
+            else if(accounts.Length == accountCount) {
+                Console.WriteLine("Array is full cannot add anymore accounts.\n");
+                return false;
+            }
+            else
+            {
+                accounts[accountCount] = inAccount;
+                accountCount++;
+                return true;
+            }
+        }
+        public Account FindAccount(string inAccountNumber)
+        /// <summary>
+        /// Purpose: searches for a account and returns it if found
+        /// parameters: a account number
+        /// Returns: the account thats found
+        {
+            for (int i = 0; i < accounts.Length;i++)
+            {
+                if (accounts[i].accountNumber == inAccountNumber)
+                {
+                    return accounts[i];
+                }
+            }
+            return null;
+
+        }
+
+        public override string ToString()
+        /// <summary>
+        /// Purpose: to print out the array of accounts and their info
+        /// Returns: a string of detailed account info
+        {
+            string tempReturn = "";
+            for(int i = 0; i <accounts.Length;i++)
+            {
+                tempReturn = tempReturn + "Name: " + accounts[i].name + "\n" +
+                    "Address: " + accounts[i].address + "\n" +
+                    "Account Number: " + accounts[i].accountNumber + "\n" +
+                    "Balance: " + accounts[i].balance + "\n\n";
+                    
+            }
+            return tempReturn;
+                
+        }
+
+    }
+}
---
+++ BankingApplication/Program.cs
@@ -0,0 +1,228 @@
+﻿// Project Prolog
+// Name: Andrew Campbell
+// CS3260 Section 001
+// Project: Lab #03 Banking Program
+// Date: 2/4/23
+// Purpose: <The purpose of the project is to learn how to use interfaces, inheritance and classes together>
+//
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes plagiarism, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+namespace BankingApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 04: Banking Application Phase 2\n");
+            AccountManager Manager = new AccountManager();//creating an account manager
+            int inValue;
+            string inName;
+            string inAddress;
+            while (true)//seeing how many accounts to enter
+            {
+                Console.WriteLine("How many accounts would you like to create? ");
+                string howMany = Console.ReadLine();
+                if (howMany == "")
+                {
+                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                }
+                
+                else if (int.TryParse(howMany, out int value))
+                {
+                    inValue = value;
+                    if(inValue > 0)
+                    {
+                        Manager.SetArrayLength(value);//setting array length
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You did not enter a correct amount please try again.\n");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                }
+            }
+            for(int j = 0; j < inValue; j++)//looping through the amount  for the array
+            {
+
+                while (true)//getting name and error validation
+                {
+                    Console.WriteLine("\nPlease enter the name for the account: ");
+                    inName = Console.ReadLine();
+                    if (inName == "")//error validation
+                    {
+                        Console.WriteLine("You did not enter a name please try again: ");
+                    }
+                    else if (int.TryParse(inName, out int value))
+                    {
+                        Console.WriteLine("You did not enter a name please try again: ");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                while (true)//getting address and error validation
+                {
+                    Console.WriteLine("\nPlease enter the Address for the account: ");
+                    inAddress = Console.ReadLine();
+                    if (inAddress == "")//error validation
+                    {
+                        Console.WriteLine("You did not enter a Address please try again: ");
+                    }
+                    else if (int.TryParse(inAddress, out int value))
+                    {
+                        Console.WriteLine("You did not enter a Address please try again: ");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                while (true)//getting what type of account and error validation
+                {
+                    Console.WriteLine("What Type of account would you like, Press: 1 - for Checkings, 2 - for Savings, 3 - for CD ");
+                    string inOption = Console.ReadLine();
+                    if (inOption == "1" || inOption == "2" || inOption == "3")//giving only 3 options to choose from
+                    {
+                        if (inOption == "1")//For Checking
+                        {
+                            while (true)// getting initial deposit and error checking
+                            {
+                                decimal finalAmount;
+                                Console.WriteLine("How much do you want to start out with?");
+                                string inDeposit = Console.ReadLine();
+                                if (inDeposit == "")
+                                {
+                                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                                }
+                                else if (decimal.TryParse(inDeposit, out decimal value))
+                                {
+                                    finalAmount = value;
+                                    if (finalAmount < 10)
+                                    {
+                                        Console.WriteLine("You need to deposit at least 10$ for checking please try again.");
+                                    }
+                                    else//setting up account
+                                    {
+                                        CheckingAccount account = new CheckingAccount();
+                                        account.CheckSetBalance(finalAmount);
+                                        account.accountServiceFee = 5;
+                                        account.CheckSetBalance(value);
+                                        account.SetName(inName);
+                                        account.SetAddress(inAddress);
+                                        account.AccountNumber();
+                                        Manager.StoreAccount(account);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                                }
+                               
+                            }
+                            break;
+                            
+                        }
+                        else if (inOption == "2")//For saving
+                        {
+                            while (true)//getting initial balance and error validation
+                            {
+                                decimal finalAmount;
+                                Console.WriteLine("How much do you want to start out with?");
+                                string inDeposit = Console.ReadLine();
+                                if (inDeposit == "")
+                                {
+                                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                                }
+                                else if (decimal.TryParse(inDeposit, out decimal value))
+                                {
+                                    finalAmount = value;
+                                    if (finalAmount < 100)
+                                    {
+                                        Console.WriteLine("You need to deposit at least 100$ for checking please try again.");
+                                    }
+                                    else//setting up account
+                                    {
+                                        SavingAccount account = new SavingAccount();
+                                        account.SaveSetBalance(finalAmount);
+                                        account.accountServiceFee = 0;
+                                        account.SaveSetBalance(value);
+                                        account.SetName(inName);
+                                        account.SetAddress(inAddress);
+                                        account.AccountNumber();
+                                        Manager.StoreAccount(account);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                                }
+
+                            }
+                            break;
+
+                        }
+                        else//For CD
+                        {
+                            while (true)//gettting initial deposit and error validation
+                            {
+                                decimal finalAmount;
+                                Console.WriteLine("How much do you want to start out with?");
+                                string inDeposit = Console.ReadLine();
+                                if (inDeposit == "")
+                                {
+                                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                                }
+                                else if (decimal.TryParse(inDeposit, out decimal value))
+                                {
+                                    finalAmount = value;
+                                    if (finalAmount < 500)
+                                    {
+                                        Console.WriteLine("You need to deposit at least 500$ for checking please try again.");
+                                    }
+                                    else//setting up account
+                                    {
+                                        CDAccount account = new CDAccount();
+                                        account.CDSetBalance(finalAmount);
+                                        account.accountServiceFee = 8;
+                                        account.CDSetBalance(value);
+                                        account.SetName(inName);
+                                        account.SetAddress(inAddress);
+                                        account.AccountNumber();
+                                        Manager.StoreAccount(account);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("You did not enter a correct amount please try again.\n");
+                                }
+
+                            }
+                            break;
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have entered an invalid option, please try again.");//error validation
+                    }
+                }
+
+                
+            }
+            Console.WriteLine("\n\nAccount Details-\n");
+            Console.WriteLine(Manager);//printing out account details
+        }
+    }
+}
